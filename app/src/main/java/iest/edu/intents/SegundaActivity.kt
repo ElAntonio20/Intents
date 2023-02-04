cd029c93c12e53c6d9909c58291e6993e8dc5a3f
ISND 8vo ISND89 9no Semestre <ISND89@AISND20-22.campus.iest.edu.mx>
2023-02-03 19:02:53 -0600

    Initial commit

---
+++ app/src/main/java/iest/edu/intents/SegundaActivity.kt
@@ -0,0 +1,14 @@
+package iest.edu.intents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+ class SegundaActivity : AppCompatActivity() {
+     override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.segunda_main)
+     }
+ }
+
+
+
